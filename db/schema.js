818b040c4c38adf1c0881d4ad84cf5cbc94d6066
jwats287 <jwats287@users.noreply.github.com>
2017-08-10 16:07:43 -0400

    Really added Schema

--- db/schema.js
+++ db/schema.js
@@ -1,38 +1,30 @@
+
 const mongoose = require('mongoose');
 
 const waterSchema = mongoose.Schema({
-  value: Number,
-  question: String,
-  answer: String
+  servingsConsumed: Number,
+  servingsDesired: Number,
 });
 
-const categorySchema = mongoose.Schema({
-  name: String,
-  questions: [questionSchema],
+const daySchema = mongoose.Schema({
+  date: Date,
+  waterRatio: [waterSchema]
 });
 
-const gameSchema = mongoose.Schema({
-  user: String,
-  points: Number,
-  board: [Boolean],
-  categories: [categorySchema],
+const userSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  weight: Number,
+  today: [daySchema]
 });
 
-gameSchema.pre('save', function(next){
-  const emptyBoard = [
-    false, false, false, false, false, false,
-    false, false, false, false, false, false,
-    false, false, false, false, false, false,
-    false, false, false, false, false, false, 
-    false, false, false, false, false, false
-  ];
-  this.board = emptyBoard;
-  next();
-})
-const Question = mongoose.model('Question', questionSchema);
-const Category = mongoose.model('Category', categorySchema);
-const Game = mongoose.model('Game', gameSchema);
+
+const Water = mongoose.model('Water', waterSchema);
+const Day = mongoose.model('Day', daySchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = {
-  Question, Category, Game
+  Water, Day, User
 }
