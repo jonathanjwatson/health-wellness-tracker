496a9bd53fd058ed4a24b60bf98a6bc8ec65dda1
jwats287 <jwats287@users.noreply.github.com>
2017-08-16 11:12:44 -0400

    Edit/Update route working

--- client/src/components/UserFoodItem.jsx
+++ client/src/components/UserFoodItem.jsx
@@ -3,6 +3,20 @@ import { Router } from 'react-router-dom';
 import axios from 'axios';
 
 class UserFoodItem extends Component {
+constructor() {
+        super();
+        this.state = {
+            showEdit: false,
+            foodItem: {
+                name: '',
+                calories: 0
+            }
+        }    
+        }
+    componentWillMount() {
+        const foodItem = this.props;
+        this.setState({foodItem})
+    }
     _removeFromDay = (e) => {
         const foodId = this.props._id;
         const userId = this.props.userId;
@@ -13,10 +27,70 @@ class UserFoodItem extends Component {
         })
         
     }
+    _editFood = (e) => {
+        const foodId = this.props._id;
+        const userId = this.props.userId;
+        console.log(`You want to edit food: ${foodId}`)
+        const showEdit = !this.state.showEdit;
+        this.setState({showEdit})
+        
+    }
+    _updateFood = (e) => {
+        e.preventDefault();
+        const foodId = this.props._id;
+        const userId = this.props.userId;
+        const payload = this.state.foodItem;
+        console.log(`User ID: ${userId}`)
+        console.log(`You want to update food: ${foodId}`)
+        axios.put(`/api/user/${userId}/editFoodItem/${foodId}`, payload)
+        .then((res) => {
+            this.props.getUserData();
+            const showEdit = !this.state.showEdit;
+            this.setState({showEdit})
+        }).catch(err => console.log(err));
+        
+    }
+    _handleChange = (e) => {
+        const attributeName = e.target.name;;
+        const attributeValue = e.target.value;
+        const foodItem = {...this.state.foodItem};
+        foodItem[attributeName] = attributeValue;
+        this.setState({ foodItem })
+    }
     render() {
         return (
             <div>
-                <p>{this.props.name} : {this.props.calories}  : {this.props._id}<button onClick={this._removeFromDay}>Remove from my day</button></p>
+                <p>{this.props.name} : {this.props.calories}  : {this.props._id}
+                <button onClick={this._removeFromDay}>Remove from my day</button>
+                </p>
+                <p>
+                <button onClick={this._editFood}>Edit Item</button>
+                </p>
+                <div>
+                    {this.state.showEdit ? 
+                                <div>
+                            <form onSubmit={this._updateFood}>
+                                <label htmlFor="name">Food Name</label>
+                                    <input 
+                                        type="text" 
+                                        onChange={this._handleChange} 
+                                        value={this.state.foodItem.name} 
+                                        name="name"
+                                        placeholder="Food Name"
+                                    />
+                            <label htmlFor="calories">Food Calories</label>
+                                    <input 
+                                        type="text" 
+                                        onChange={this._handleChange} 
+                                        value={this.state.foodItem.calories} 
+                                        name="calories"
+                                        placeholder="Food Calories"
+                                    />
+                            <button>Update Food</button>
+                            </form>
+                        </div>
+                    : null}
+                </div>
             </div>
         );
     }
--- controllers/user.js
+++ controllers/user.js
@@ -11,12 +11,37 @@ router.get("/", (req, res) => {
   });
 });
 
+
 router.put("/addNewServingConsumed/:id", (req, res) => {
   User.findById(req.params.id).then((user) => {
     user.today[0].waterRatio[0].servingsConsumed +=1;
     return user.save();
   })
 })
+
+router.put("/:userId/editFoodItem/:foodId", (req, res) => {
+  User.findById(req.params.userId).then((user) => {
+    console.log(req.body)
+    console.log(user)
+    const foodId = req.params.foodId
+    const foodConsumed = user.today[0].foodConsumed
+    console.log(`==========foodConsumed==============`)
+    console.log(foodConsumed);
+
+    const foundFood = foodConsumed.findIndex((food) => {
+      console.log(food.id)
+      console.log(foodId);
+      return food.id === foodId
+    })
+    user.today[0].foodConsumed[foundFood].name = req.body.name;
+    user.today[0].foodConsumed[foundFood].calories = req.body.calories;
+    return user.save();
+  })
+  .then((user) => {
+    res.json(user)
+  })
+})
+
 
 router.post("/email/:email", (req, res) => {
   const userEmail = req.params.email;
